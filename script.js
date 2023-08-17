0ddaa16c9b0536ae3b8581565cacac53d32dddb3
SyedMasood46 <masoodhussaini46@gmail.com>
2023-08-17 12:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let mp={ };
+let arr2=[];
+for(let i=0;i<touristSpots.length;i++){
+	let words=touristSpots[i].split(" ");
+	let finalString="";
+	for(let i=0;i<words.length;i++){
+		let currwords=words[i].toLowerCase();
+		if(currwords !=='an' && currwords!=='a' && currwords!=='the'){
+			finalString+=words[i]+" ";
+		}
+	}
+	let articlelessStr=finalString.trim();
+	mp[arr[i]]=articlelessStr;
+	arr2.push(articlelessStr);
+	arr2.sort();
+}
